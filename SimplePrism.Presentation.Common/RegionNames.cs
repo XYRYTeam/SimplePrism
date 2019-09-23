e86af6316d8850f8ef401695fc7d100cb2aec626
Chemmy <chengming0916@gmail.com>
2019-09-23 21:53:28 +0800

    Region名称统一管理

---
+++ SimplePrism.Presentation.Common/RegionNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrism.Presentation.Common
+{
+    /// <summary>
+    /// Region命名
+    /// </summary>
+    public static class RegionNames
+    {
+        //private RegionNames() { }
+
+        public const string MainRegion = "MainRegion";
+    }
+}

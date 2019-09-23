96f8b4d6cdbeb90ea57d505a827c8d6ac7f150d4
Chemmy <chengming0916@gmail.com>
2019-09-23 21:52:40 +0800

    移除无用界面

---
+++ SimplePrism.Modules.UserModule/UserModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using SimplePrism.Presentation.Common;
+
+namespace SimplePrism.Modules.UserModule
+{
+    public class UserModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public UserModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+        }
+    }
+}

f41725a689fdbae9f42db1f43c64a24a38d0aa93
Chemmy <chengming0916@gmail.com>
2019-09-23 21:53:44 +0800

    Shell实现

---
+++ SimplePrism.Presentation/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Unity;
+using System.Windows;
+
+namespace SimplePrism.Presentation
+{
+    internal class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+            //return base.CreateShell();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+            //base.InitializeShell();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new DirectoryModuleCatalog()
+            {
+                ModulePath = @".\Modules"
+            };
+            //return base.CreateModuleCatalog();
+        }
+    }
+}

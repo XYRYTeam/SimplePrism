4193b13cc65db5d0902d3c375dea568cb73c946c
Chemmy <chengming0916@gmail.com>
2019-09-23 21:53:54 +0800

    管理模块

---
+++ SimplePrism.Modules.ManagementModule/ManagementModule.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using SimplePrism.Modules.ManagementModule.Views;
+using SimplePrism.Presentation.Common;
+
+namespace SimplePrism.Modules.ManagementModule
+{
+    public class ManagementModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public ManagementModule(RegionManager regionManager)
+        {
+            this._regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion(RegionNames.MainRegion, typeof(LoginScreen));
+        }
+    }
+}

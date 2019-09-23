3829b2d900e4d08d215d6df972c80a3d2caf5651
Chemmy <chengming0916@gmail.com>
2019-09-23 21:54:34 +0800

    设置模块

---
+++ SimplePrism.Modules.SettingModule/SettingModule.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using Unity;
+
+namespace SimplePrism.Modules.SettingModule
+{
+    public class SettingModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+        private readonly IUnityContainer container;
+
+        public SettingModule(RegionManager regionManager, IUnityContainer container)
+        {
+            this.regionManager = regionManager;
+            this.container = container;
+        }
+
+        public void Initialize()
+        {
+            container.RegisterType<Views.ViewA>();
+            container.RegisterType<ViewModels.ViewAViewModel>();
+        }
+    }
+}
